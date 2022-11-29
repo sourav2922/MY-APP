e7ea40e45c71b0726ad58cabb19f7f745b065dd6
sourav2922 <souravyadav040@gmail.com>
2022-11-30 01:14:32 +0530

    Created File

---
+++ MY-APP/MY-APP/MY_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MY_APPApp.swift
+//  MY-APP
+//
+//  Created by sourav on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MY_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
